41b07553bc5a6709a787a33fc24105e7985bb797
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-28 10:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let element=ducument.getElementById("Level");
+let n=0;
+while(element)
+	{
+		n++;
+		element=element.parentElement;
+	}
+
+console.log("The level of the element is: " n);
